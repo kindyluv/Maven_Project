3efa90066eb133c509a8a3123345bb15fe323f81
kindyluv <onyeukwuamara@gmail.com>
2021-10-12 17:47:34 +0100

    CustomerRepository added

---
+++ BankingApplication/src/main/java/africa/semicolon/BankingApplication/data/repositories/CustomerRepository.java
@@ -0,0 +1,15 @@
+package africa.semicolon.BankingApplication.data.repositories;
+
+import africa.semicolon.BankingApplication.data.models.Customer;
+
+import java.util.List;
+
+public interface CustomerRepository {
+    Customer save(Customer customer);
+    Customer findById(String id);
+    void delete(Customer customer);
+    void delete(String id);
+    List<Customer> findAll();
+    
+    // TODO create other repos against tomorrow
+}
