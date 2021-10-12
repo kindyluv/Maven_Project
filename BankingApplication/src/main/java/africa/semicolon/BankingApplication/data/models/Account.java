bf43947c5e9494953170a42109e88e8ca8341cfc
kindyluv <onyeukwuamara@gmail.com>
2021-10-12 17:44:44 +0100

    Account added

---
+++ BankingApplication/src/main/java/africa/semicolon/BankingApplication/data/models/Account.java
@@ -0,0 +1,13 @@
+package africa.semicolon.BankingApplication.data.models;
+
+import lombok.Data;
+import java.math.BigDecimal;
+
+
+@Data
+public class Account {
+    private String customerId;
+    private String number;
+    private AccountType type;
+    private BigDecimal balance = BigDecimal.ZERO;
+}
