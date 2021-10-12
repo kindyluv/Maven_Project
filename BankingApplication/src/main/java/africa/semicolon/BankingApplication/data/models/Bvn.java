1b76641701a95218f40612d27d493d11759c134c
kindyluv <onyeukwuamara@gmail.com>
2021-10-12 17:46:14 +0100

    Bvn added

---
+++ BankingApplication/src/main/java/africa/semicolon/BankingApplication/data/models/Bvn.java
@@ -0,0 +1,10 @@
+package africa.semicolon.BankingApplication.data.models;
+
+import lombok.Data;
+
+
+@Data
+public class Bvn {
+    private final String id;
+    private final Customer customer;
+}
