c5b48afafe87431572b5e9bfcdfacfa1bc57d142
kindyluv <onyeukwuamara@gmail.com>
2021-10-13 17:13:05 +0100

    Made changes to the customer service impl class

--- BankingApplication/src/main/java/africa/semicolon/BankingApplication/services/CustomerServiceImpl.java
+++ BankingApplication/src/main/java/africa/semicolon/BankingApplication/services/CustomerServiceImpl.java
@@ -1,18 +1,28 @@
 package africa.semicolon.BankingApplication.services;
 
-import africa.semicolon.BankingApplication.data.models.Account;
+import africa.semicolon.BankingApplication.data.models.Customer;
 import africa.semicolon.BankingApplication.data.repositories.CustomerRepository;
 import africa.semicolon.BankingApplication.data.repositories.CustomerRepositoryImpl;
+import africa.semicolon.BankingApplication.dtos.response.CustomerResponse;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CustomerServiceImpl implements CustomerService{
     private final CustomerRepository customerRepository = new CustomerRepositoryImpl();
-    @Override
-    public String createAccount(String firstName, String lastName) {
-        return null;
-    }
 
     @Override
-    public String createAccount(String firstName, String lastName, String bvn) {
-        return null;
+    public List<CustomerResponse> findAll() {
+        List<CustomerResponse> responses = new ArrayList<>();
+        List<Customer> ourRealCustomers = customerRepository.findAll();
+        for (Customer customer : ourRealCustomers) {
+            CustomerResponse customerResponse = new CustomerResponse();
+            customerResponse.setFirstName(customer.getFirstName());
+            customerResponse.setLastName(customer.getLastName());
+            customerResponse.setBvn(customer.getBvn());
+            customerResponse.setNumberOfAccounts(customer.getAccounts().size());
+            responses.add(customerResponse);
+        }
+        return responses;
     }
 }
