fff2069d266d4148004b54991f5a7885cb15161d
kindyluv <onyeukwuamara@gmail.com>
2021-10-13 10:09:37 +0100

    CustomerServiceImpl added

---
+++ BankingApplication/src/main/java/africa/semicolon/BankingApplication/data/repositories/BankRepositoryImp.java
@@ -0,0 +1,42 @@
+package africa.semicolon.BankingApplication.data.repositories;
+
+import africa.semicolon.BankingApplication.data.models.Account;
+import africa.semicolon.BankingApplication.data.models.Bank;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BankRepositoryImp implements BankRepository {
+    List<Bank> banks = new ArrayList<>();
+
+
+    @Override
+    public Bank save(Bank bank) {
+        banks.add(bank);
+        return findBankById(bank.getId());
+    }
+
+    @Override
+    public Bank findBankById(String id) {
+        for(Bank bank : banks) {
+            if(bank.getId().equalsIgnoreCase(id)) return bank;
+        }
+        return null;
+    }
+
+    @Override
+    public void delete(Bank bank) {
+        banks.remove(bank);
+    }
+
+    @Override
+    public void delete(String id) {
+        Bank bank = findBankById(id);
+        banks.remove(bank);
+    }
+
+    @Override
+    public List<Bank> findAll() {
+        return banks;
+    }
+}
