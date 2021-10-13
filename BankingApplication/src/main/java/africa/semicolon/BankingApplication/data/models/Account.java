168fced3a434432f3c28c31483aad88aef4e4548
kindyluv <onyeukwuamara@gmail.com>
2021-10-13 14:22:33 +0100

    Made changes to the Account class

--- BankingApplication/src/main/java/africa/semicolon/BankingApplication/data/models/Account.java
+++ BankingApplication/src/main/java/africa/semicolon/BankingApplication/data/models/Account.java
@@ -3,10 +3,12 @@ package africa.semicolon.BankingApplication.data.models;
 import lombok.Data;
 import java.math.BigDecimal;
 
+import static africa.semicolon.BankingApplication.data.models.AccountType.SAVINGS;
+
 @Data
 public class Account {
-    private String customerId;
+    private String customerId = "0";
     private String number;
-    private AccountType type;
+    private AccountType type = SAVINGS;
     private BigDecimal balance = BigDecimal.ZERO;
 }
