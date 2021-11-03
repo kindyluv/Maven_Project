27148bd5193261de2f530a2c19eb2b1fcf9bfa13
kindyluv <onyeukwuamara@gmail.com>
2021-11-03 16:21:22 +0100

    ...

---
+++ cheetah/src/main/java/africa/semicolon/cheetah/services/PackageServiceImpl.java
@@ -0,0 +1,11 @@
+package africa.semicolon.cheetah.services;
+
+import africa.semicolon.cheetah.dtos.request.AddPackageRequest;
+import africa.semicolon.cheetah.dtos.responses.AddPackageResponse;
+
+public class PackageServiceImpl implements PackageService {
+    @Override
+    public AddPackageResponse addPackage(AddPackageRequest addPackageRequest) {
+        return null;
+    }
+}
