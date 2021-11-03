74e611d7c643a9cf6aef26a6628960ef7306da4a
kindyluv <onyeukwuamara@gmail.com>
2021-11-03 16:22:13 +0100

    ...

---
+++ cheetah/src/main/java/africa/semicolon/cheetah/dtos/request/AddPackageRequest.java
@@ -0,0 +1,14 @@
+package africa.semicolon.cheetah.dtos.request;
+
+import lombok.Data;
+
+@Data
+public class AddPackageRequest {
+    private String sendersName;
+    private String sendersPhone;
+    private String receiversName;
+    private String deliveryAddress;
+    private double weight;
+    private String packageDescription;
+    private String receiverPhone;
+}
