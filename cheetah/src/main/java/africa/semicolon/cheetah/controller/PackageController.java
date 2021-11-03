9fd6776dedc23eb1ff452dc8973a609ad15b7927
kindyluv <onyeukwuamara@gmail.com>
2021-11-03 18:48:15 +0100

    ...

--- cheetah/src/main/java/africa/semicolon/cheetah/controller/PackageController.java
+++ cheetah/src/main/java/africa/semicolon/cheetah/controller/PackageController.java
@@ -2,16 +2,25 @@ package africa.semicolon.cheetah.controller;
 
 import africa.semicolon.cheetah.data.repositories.PackageRepository;
 import africa.semicolon.cheetah.data.repositories.PackageRepositoryImpl;
+import africa.semicolon.cheetah.data.models.Package;
 import africa.semicolon.cheetah.dtos.request.AddPackageRequest;
 import africa.semicolon.cheetah.dtos.responses.AddPackageResponse;
 import africa.semicolon.cheetah.services.PackageService;
 import africa.semicolon.cheetah.services.PackageServiceImpl;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
 public class PackageController {
     private final PackageService packageService = new PackageServiceImpl();
     private PackageRepository packageRepository = new PackageRepositoryImpl();
 
-    public AddPackageResponse addPackage(AddPackageRequest addPackageRequest) {
+    @PostMapping("/api/addpackage")
+    public AddPackageResponse addPackage(@RequestBody AddPackageRequest addPackageRequest) {
         return packageService.addPackage(addPackageRequest);
+    }
+
+    @GetMapping("/api/package/{id}")
+    public Package findPackageId(@PathVariable("id") Integer id){
+        return packageService.findPackageWithMy(id);
     }
 }
