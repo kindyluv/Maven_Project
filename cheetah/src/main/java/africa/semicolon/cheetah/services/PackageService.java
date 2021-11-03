00bd3f5a8f1072dbd975768c053c641c32414803
kindyluv <onyeukwuamara@gmail.com>
2021-11-03 18:48:23 +0100

    ...

--- cheetah/src/main/java/africa/semicolon/cheetah/services/PackageService.java
+++ cheetah/src/main/java/africa/semicolon/cheetah/services/PackageService.java
@@ -1,9 +1,12 @@
 package africa.semicolon.cheetah.services;
 
+import africa.semicolon.cheetah.data.models.Package;
 import africa.semicolon.cheetah.dtos.request.AddPackageRequest;
 import africa.semicolon.cheetah.dtos.responses.AddPackageResponse;
 
 public interface PackageService {
 
     AddPackageResponse addPackage(AddPackageRequest addPackageRequest);
+
+    Package findPackageWithMy(Integer id);
 }
