933b11242b84cdd2096e5dffa6ccb8553bd6f409
kindyluv <onyeukwuamara@gmail.com>
2021-11-03 17:25:50 +0100

    ...

--- cheetah/src/main/java/africa/semicolon/cheetah/data/models/Package.java
+++ cheetah/src/main/java/africa/semicolon/cheetah/data/models/Package.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class Package {
-    private Integer trackingNumber;
+    private Integer id;
     private String name;
     private String senderName;
     private String receiverName;
