c768545f44abe90bb4e6cd6a3f12d458a0dd0f87
kindyluv <onyeukwuamara@gmail.com>
2021-11-03 17:48:29 +0100

    ...

--- cheetah/src/main/java/africa/semicolon/cheetah/services/PackageServiceImpl.java
+++ cheetah/src/main/java/africa/semicolon/cheetah/services/PackageServiceImpl.java
@@ -1,11 +1,27 @@
 package africa.semicolon.cheetah.services;
 
+import africa.semicolon.cheetah.data.models.Package;
+import africa.semicolon.cheetah.data.repositories.PackageRepository;
+import africa.semicolon.cheetah.data.repositories.PackageRepositoryImpl;
 import africa.semicolon.cheetah.dtos.request.AddPackageRequest;
 import africa.semicolon.cheetah.dtos.responses.AddPackageResponse;
+import africa.semicolon.cheetah.utils.ModelMapper;
 
 public class PackageServiceImpl implements PackageService {
+    private final PackageRepository packageRepository = new PackageRepositoryImpl();
+
     @Override
     public AddPackageResponse addPackage(AddPackageRequest addPackageRequest) {
-        return null;
+        //convert addPackage request to a package
+        Package aPackage = ModelMapper.map(addPackageRequest);
+
+
+        //save package
+        Package savePackage = packageRepository.save(aPackage);
+        //convert saved package to addPackage response
+        AddPackageResponse response = ModelMapper.map(savePackage);
+
+        //return converted response
+        return response;
     }
 }
