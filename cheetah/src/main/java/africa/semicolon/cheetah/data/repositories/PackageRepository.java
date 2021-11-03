6be3b50d85bfcc67e0ec2c248f907742d1a78a1f
kindyluv <onyeukwuamara@gmail.com>
2021-11-03 17:26:28 +0100

    ...

--- cheetah/src/main/java/africa/semicolon/cheetah/data/repositories/PackageRepository.java
+++ cheetah/src/main/java/africa/semicolon/cheetah/data/repositories/PackageRepository.java
@@ -2,12 +2,11 @@ package africa.semicolon.cheetah.data.repositories;
 import africa.semicolon.cheetah.data.models.Package;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface PackageRepository {
     Package save(Package aPackage);
     List<Package> findAll();
     void delete(Package aPackage);
-    void deleteByTrackingNumber(Integer trackingNumber);
-    Optional<Package> findPackageTrackingNumbered(Integer trackingNumber);
+    void delete(Integer trackingNumber);
+    Package findPackageTrackingNumbered(Integer trackingNumber);
 }
