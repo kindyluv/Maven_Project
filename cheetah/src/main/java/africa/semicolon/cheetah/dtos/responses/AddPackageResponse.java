dde73a0de00ac1d2809bfb3396cb300933fab9ca
kindyluv <onyeukwuamara@gmail.com>
2021-11-03 17:25:35 +0100

    ...

--- cheetah/src/main/java/africa/semicolon/cheetah/dtos/responses/AddPackageResponse.java
+++ cheetah/src/main/java/africa/semicolon/cheetah/dtos/responses/AddPackageResponse.java
@@ -1,4 +1,12 @@
 package africa.semicolon.cheetah.dtos.responses;
 
+import lombok.Data;
+
+@Data
 public class AddPackageResponse {
+    private Integer trackingNumber;
+    private String PackageName;
+    private String receiverName;
+    private String receiverPhone;
+    private double packageWeight;
 }
