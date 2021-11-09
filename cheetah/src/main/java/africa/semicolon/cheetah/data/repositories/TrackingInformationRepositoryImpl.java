3bce5a2a66a437647106a351fae1153e25db4742
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-11-09 16:49:45 +0100

    Add files via upload

---
+++ cheetah/src/main/java/africa/semicolon/cheetah/data/repositories/TrackingInformationRepositoryImpl.java
@@ -0,0 +1,46 @@
+package africa.semicolon.cheetah.data.repositories;
+
+import africa.semicolon.cheetah.data.models.TrackingInformation;
+
+import java.util.*;
+
+public class TrackingInformationRepositoryImpl implements TrackingInformationRepository {
+    private Map<Integer, TrackingInformation> database = new HashMap<>();
+
+    @Override
+    public TrackingInformation save(TrackingInformation trackingInformation) {
+        database.put(trackingInformation.getPackageId(),trackingInformation);
+        return trackingInformation;
+    }
+
+    @Override
+    public List<TrackingInformation> findAll() {
+        List<TrackingInformation> all = new ArrayList<>();
+        Set<Integer> keys = database.keySet();
+        for (Integer key: keys) {
+            all.add(database.get(key));
+        }
+        return all;
+    }
+
+    @Override
+    public void deleteAll() {
+        database.clear();
+    }
+
+    @Override
+    public Optional<TrackingInformation> findByPackageId(Integer packageId) {
+        if (database.containsKey(packageId)) return Optional.of(database.get(packageId));
+        return Optional.empty();
+    }
+
+    @Override
+    public void delete(Integer packageId) {
+        database.remove(packageId);
+    }
+
+    @Override
+    public void delete(TrackingInformation trackingInformation) {
+        delete(trackingInformation.getPackageId());
+    }
+}
