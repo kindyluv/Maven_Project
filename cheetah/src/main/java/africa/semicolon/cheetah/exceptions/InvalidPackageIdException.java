45aef3657743cbf7aa869b6bc068e819b638fe56
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-11-09 18:07:28 +0100

    Add files via upload

---
+++ cheetah/src/main/java/africa/semicolon/cheetah/exceptions/InvalidPackageIdException.java
@@ -0,0 +1,7 @@
+package africa.semicolon.cheetah.exceptions;
+
+public class InvalidPackageIdException extends CheetarAppException {
+    public InvalidPackageIdException(String message) {
+        super(message);
+    }
+}
