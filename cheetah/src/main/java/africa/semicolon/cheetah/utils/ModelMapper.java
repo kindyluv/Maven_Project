e83946ed0dc3d4ee8b9f3cf6c9f6c7b8257595e5
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-11-09 18:05:12 +0100

    Add files via upload

--- cheetah/src/main/java/africa/semicolon/cheetah/utils/ModelMapper.java
+++ cheetah/src/main/java/africa/semicolon/cheetah/utils/ModelMapper.java
@@ -1,6 +1,12 @@
 package africa.semicolon.cheetah.utils;
 
 import africa.semicolon.cheetah.data.models.Package;
+import africa.semicolon.cheetah.data.models.Sender;
+import africa.semicolon.cheetah.data.models.TrackingData;
+import africa.semicolon.cheetah.dtos.responses.AddTrackingInfoResponse;
+import africa.semicolon.cheetah.dtos.request.AddTrackingInfoRequest;
+import africa.semicolon.cheetah.dtos.request.RegisterSenderRequest;
+import africa.semicolon.cheetah.dtos.responses.RegisterSenderResponse;
 import africa.semicolon.cheetah.dtos.request.AddPackageRequest;
 import africa.semicolon.cheetah.dtos.responses.AddPackageResponse;
 
@@ -8,11 +14,10 @@ public class ModelMapper {
 
     public static Package map(AddPackageRequest addPackageRequest){
         Package aPackage = new Package();
+
         aPackage.setName(addPackageRequest.getPackageDescription());
         aPackage.setDeliveryAddress(addPackageRequest.getDeliveryAddress());
-        aPackage.setSenderName(addPackageRequest.getSendersPhone());
-        aPackage.setSenderPhone(addPackageRequest.getSendersPhone());
-        aPackage.setReceiverPhone(addPackageRequest.getReceiverPhone());
+        aPackage.setSenderEmail(addPackageRequest.getSenderEmail());
         aPackage.setReceiverName(addPackageRequest.getReceiversName());
         aPackage.setNetWeight(addPackageRequest.getWeight());
 
@@ -29,5 +34,27 @@ public class ModelMapper {
         response.setTrackingNumber(savePackage.getId());
 
         return response;
+    }
+
+    public static Sender map(RegisterSenderRequest request){
+        Sender sender = new Sender();
+        sender.setSenderName(request.getSenderName());
+        sender.setPhoneNumber(request.getPhoneNumber());
+        sender.setEmailAddress(request.getSenderEmail());
+        return sender;
+    }
+
+    public static RegisterSenderResponse map(Sender sender){
+        RegisterSenderResponse response = new RegisterSenderResponse();
+        response.setSenderEmail(sender.getEmailAddress());
+        return response;
+    }
+
+    public static AddTrackingInfoResponse map(TrackingData trackingData, AddTrackingInfoRequest addTrackingInfo) {
+        AddTrackingInfoResponse addTrackingInfoResponse = new AddTrackingInfoResponse();
+        addTrackingInfoResponse.setDateTime(trackingData.getDataTimeOfEvent());
+        addTrackingInfoResponse.setEvent(trackingData.getEvent());
+        addTrackingInfoResponse.setPackageId(addTrackingInfo.getPackageId());
+        return addTrackingInfoResponse;
     }
 }
