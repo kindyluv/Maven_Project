30d20c63957d7a7e5ba9a958ff26d82c5c7a139b
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-11-09 16:49:13 +0100

    Add files via upload

---
+++ cheetah/src/main/java/africa/semicolon/cheetah/data/repositories/TrackingInformationRepository.java
@@ -0,0 +1,15 @@
+package africa.semicolon.cheetah.data.repositories;
+
+import africa.semicolon.cheetah.data.models.TrackingInformation;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TrackingInformationRepository {
+    TrackingInformation save(TrackingInformation trackingInformation);
+    List<TrackingInformation> findAll();
+    void deleteAll();
+    Optional<TrackingInformation> findByPackageId(Integer packageId);
+    void delete(Integer packageId);
+    void delete(TrackingInformation trackingInformation);
+}
