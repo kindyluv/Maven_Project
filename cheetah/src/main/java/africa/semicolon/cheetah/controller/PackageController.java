30709582e8f82e1c526df36ac86d7e04cbbf4827
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-11-09 16:35:06 +0100

    Add files via upload

--- cheetah/src/main/java/africa/semicolon/cheetah/controller/PackageController.java
+++ cheetah/src/main/java/africa/semicolon/cheetah/controller/PackageController.java
@@ -5,8 +5,12 @@ import africa.semicolon.cheetah.data.repositories.PackageRepositoryImpl;
 import africa.semicolon.cheetah.data.models.Package;
 import africa.semicolon.cheetah.dtos.request.AddPackageRequest;
 import africa.semicolon.cheetah.dtos.responses.AddPackageResponse;
+import africa.semicolon.cheetah.exceptions.SenderNotFoundException;
 import africa.semicolon.cheetah.services.PackageService;
 import africa.semicolon.cheetah.services.PackageServiceImpl;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,8 +19,13 @@ public class PackageController {
     private PackageRepository packageRepository = new PackageRepositoryImpl();
 
     @PostMapping("/api/addpackage")
-    public AddPackageResponse addPackage(@RequestBody AddPackageRequest addPackageRequest) {
-        return packageService.addPackage(addPackageRequest);
+    public ResponseEntity<?> addPackage(@RequestBody AddPackageRequest addPackageRequest) {
+        try {
+            AddPackageResponse response = packageService.addPackage(addPackageRequest);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }catch (SenderNotFoundException e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping("/api/package/{id}")
