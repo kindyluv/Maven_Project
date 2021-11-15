562ca7923ad3e09b13688ffecb80fd9e6684a383
kindyluv <onyeukwuamara@gmail.com>
2021-11-16 00:46:23 +0100

    ..

--- bloggingProject/src/test/java/africa/semicolon/bloggingProject/data/repository/AuthorRepositoryImplTest.java
+++ bloggingProject/src/test/java/africa/semicolon/bloggingProject/data/repository/AuthorRepositoryImplTest.java
@@ -18,9 +18,9 @@ class AuthorRepositoryImplTest {
         Author author = new Author();
         author.setFirstName("Toye");
         author.setLastName("David");
-        author.setUserName(author.getFirstName() + " " + author.getLastName());
+        author.setUserName(author.getFirstName() + " @ " + author.getLastName());
         author.setAuthorId(1990);
-        author.setEmailAddress("toye@gmail.com");
+        author.setEmailAddress(author.getLastName()+"@gmail.com");
 
         return author;
     }
@@ -32,28 +32,31 @@ class AuthorRepositoryImplTest {
         author.setLastName("David");
         author.setUserName(author.getFirstName() + " " + author.getLastName());
         author.setAuthorId(1990);
-        author.setEmailAddress("toye@gmail.com");
+        author.setEmailAddress(author.getLastName()+"@gmail.com");
 
         authorRepository.saveAuthor(author);
-        assertEquals(1, authorRepository.getCount());
+        assertEquals(1, authorRepository.findAllAuthor().size());
     }
 
     @Test
     void findAuthorById() {
         Author author = response();
-        Author author__ = response();
         authorRepository.saveAuthor(author);
-        System.out.println(author);
-        author__.setAuthorId(2000);
-        author__.getAuthorId();
-        authorRepository.saveAuthor(author__);
-        System.out.println(author__);
 
-//        assertEquals(, );
+        Author author__ = authorRepository.findAuthorById(author.getAuthorId());
+        Author author_ = authorRepository.saveAuthor(author__);
+
+        assertEquals(1990, author_.getAuthorId());
     }
 
     @Test
     void findAuthorByUsername() {
+        Author author = response();
+
+        authorRepository.saveAuthor(author);
+        Author author__ = authorRepository.findAuthorByUsername(author.getUserName());
+        assertEquals("Toye @ David", author__.getUserName());
+
     }
 
     @Test
@@ -63,22 +66,34 @@ class AuthorRepositoryImplTest {
     @Test
     void findAllAuthor() {
         Author author = response();
-        Author author__ = response();
         authorRepository.saveAuthor(author);
-        authorRepository.saveAuthor(author__);
 
-        assertEquals(2, authorRepository.getCount());
+        assertEquals(1, authorRepository.findAllAuthor().size());
     }
 
     @Test
     void deleteAuthor() {
+        Author author = response();
+        Author author_ = authorRepository.saveAuthor(author);
+
+        authorRepository. deleteAuthorId(author_.getAuthorId());
+        assertEquals(0, authorRepository.findAllAuthor().size());
     }
 
     @Test
     void deleteAllAuthor() {
-    }
+        Author author1 = response();
+        Author author_ = authorRepository.saveAuthor(author1);
 
-    @Test
-    void deleteAuthorByUsername() {
+        Author author = new Author();
+        author.setFirstName("Lois");
+        author.setLastName("Ama");
+        author.setUserName(author.getFirstName() + " " + author.getLastName());
+        author.setAuthorId(9690);
+        author.setEmailAddress(author.getLastName()+"@gmail.com");
+
+        authorRepository.saveAuthor(author);
+        authorRepository.deleteAllAuthor();
+        assertEquals(0, authorRepository.findAllAuthor().size());
     }
 }
