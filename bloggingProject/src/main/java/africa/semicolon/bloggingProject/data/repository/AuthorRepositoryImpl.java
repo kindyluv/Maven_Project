599bace4e20bf048067b3dfe22328bcd981867c2
kindyluv <onyeukwuamara@gmail.com>
2021-11-16 00:46:00 +0100

    ..

--- bloggingProject/src/main/java/africa/semicolon/bloggingProject/data/repository/AuthorRepositoryImpl.java
+++ bloggingProject/src/main/java/africa/semicolon/bloggingProject/data/repository/AuthorRepositoryImpl.java
@@ -2,36 +2,41 @@ package africa.semicolon.bloggingProject.data.repository;
 
 import africa.semicolon.bloggingProject.data.model.Author;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class AuthorRepositoryImpl implements AuthorRepository{
     Map<Integer, Author> database = new HashMap<Integer, Author>();
-    private Author author = new Author();
-    private int count = 0;
 
     @Override
     public Author saveAuthor(Author author) {
         Integer authorTrackingId = null;
-        if(author.getAuthorId() != null) {
+        if(author.getAuthorId() == null) {
             authorTrackingId = database.size()+1;
             author.setAuthorId(authorTrackingId);
+        }else
+            if(author.getAuthorId()!=null ){
+            authorTrackingId = author.getAuthorId();
+            database.put(authorTrackingId, author);
         }
-        authorTrackingId = author.getAuthorId();
-        database.put(authorTrackingId, author);
-        count++;
         return database.get(authorTrackingId);
     }
 
     @Override
     public Author findAuthorById(Integer authorId) {
+        if(database.containsKey(authorId)){
+            return database.get(authorId);
+        }
         return null;
     }
 
     @Override
     public Author findAuthorByUsername(String username) {
-        return null;
+       for (Author value: database.values()) {
+           if (value.getUserName().equals(username)) {
+               return value;
+           }
+       }
+       return null;
     }
 
     @Override
@@ -46,26 +51,26 @@ public class AuthorRepositoryImpl implements AuthorRepository{
 
     @Override
     public List<Author> findAllAuthor() {
-        return null;
+        List<Author> allAuthor = new ArrayList<Author>();
+        Set<Integer> keys = database.keySet();
+        for (Integer key : keys) {
+            allAuthor.add(database.get(key));
+        }
+        return allAuthor;
     }
 
     @Override
-    public void deleteAuthor(Author author) {
-
+    public void deleteAuthorId(Integer id) {
+        for(Author value: database.values()){
+            if(value.getAuthorId().equals(id)){
+                database.remove(id);
+            }
+        }
     }
 
     @Override
-    public void deleteAllAuthor(Author... author) {
-
+    public void deleteAllAuthor() {
+       database.clear();
     }
 
-    @Override
-    public void deleteAuthorByUsername(String username) {
-
-    }
-
-    @Override
-    public int getCount() {
-        return count;
-    }
 }
