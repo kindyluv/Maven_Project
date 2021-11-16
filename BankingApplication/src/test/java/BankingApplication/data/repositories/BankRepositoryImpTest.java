84a68d337c872ee2fe68cd7f76c47a59aee98099
kindyluv <onyeukwuamara@gmail.com>
2021-11-16 16:22:09 +0100

    ..

---
+++ BankingApplication/src/test/java/BankingApplication/data/repositories/BankRepositoryImpTest.java
@@ -0,0 +1,77 @@
+package BankingApplication.data.repositories;
+
+import africa.semicolon.BankingApplication.data.models.Bank;
+import africa.semicolon.BankingApplication.data.repositories.BankRepository;
+import africa.semicolon.BankingApplication.data.repositories.BankRepositoryImp;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class BankRepositoryImpTest {
+    BankRepository bankRepository;
+
+    @BeforeEach
+    void setUp() {
+        bankRepository = new BankRepositoryImp();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void save() {
+        Bank bank = new Bank("1234567");
+        bank.setBankName("Lois");
+        Bank banking = bankRepository.save(bank);
+        assertEquals(bank, banking);
+    }
+
+    @Test
+    void findById() {
+        Bank bank = new Bank("1234567");
+        bank.setBankName("Lois");
+        bankRepository.save(bank);
+
+        Bank news = bankRepository.findBankById("1234567");
+        assertEquals(bank, news);
+    }
+
+    @Test
+    void delete() {
+        Bank bank = new Bank("1234567");
+        bank.setBankName("Lois");
+        bankRepository.save(bank);
+        bankRepository.findBankById("1234567");
+
+        bankRepository.delete(bank);
+        assertNull(bankRepository.findBankById("1234567"));
+    }
+
+    @Test
+    void deleteById() {
+        Bank bank = new Bank("1234567");
+        bank.setBankName("Lois");
+        bankRepository.save(bank);
+        bankRepository.findBankById("1234567");
+
+        bankRepository.delete("1234567");
+        assertNull(bankRepository.findBankById("1234567"));
+    }
+
+    @Test
+    void findAll() {
+        Bank bank = new Bank("1234567");
+        bank.setBankName("Lois");
+        bankRepository.save(bank);
+        bankRepository.save(bank);
+        bankRepository.save(bank);
+        bankRepository.save(bank);
+        bankRepository.save(bank);
+
+        assertEquals(5, bankRepository.findAll().size());
+    }
+}
