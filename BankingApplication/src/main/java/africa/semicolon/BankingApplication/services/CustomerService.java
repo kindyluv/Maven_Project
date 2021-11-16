2ba4040830c0555eb3a99731392919c5b8be7b98
kindyluv <onyeukwuamara@gmail.com>
2021-11-16 16:23:02 +0100

    ..

--- BankingApplication/src/main/java/africa/semicolon/BankingApplication/services/CustomerService.java
+++ BankingApplication/src/main/java/africa/semicolon/BankingApplication/services/CustomerService.java
@@ -1,10 +1,8 @@
 package africa.semicolon.BankingApplication.services;
 
-import africa.semicolon.BankingApplication.data.models.Account;
 import africa.semicolon.BankingApplication.data.models.Customer;
 import africa.semicolon.BankingApplication.dtos.response.CustomerResponse;
 
-import java.util.Collection;
 import java.util.List;
 
 public interface CustomerService {
