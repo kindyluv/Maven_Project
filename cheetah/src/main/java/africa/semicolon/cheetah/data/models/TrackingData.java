636541b9d6dfdc609918028aab3c876b6c9fb806
kindyluv <onyeukwuamara@gmail.com>
2021-11-16 11:33:44 +0100

    ..

--- cheetah/src/main/java/africa/semicolon/cheetah/data/models/TrackingData.java
+++ cheetah/src/main/java/africa/semicolon/cheetah/data/models/TrackingData.java
@@ -4,9 +4,13 @@ import org.springframework.cglib.core.Local;
 
 import java.time.LocalDateTime;
 import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Data
+@RequiredArgsConstructor
 public class TrackingData {
     private LocalDateTime dataTimeOfEvent = LocalDateTime.now();
+    @NonNull
     private String event;
 }
