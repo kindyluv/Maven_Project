3b6d89430f7fb5d8dcff360d31c605bd9153ae44
kindyluv <onyeukwuamara@gmail.com>
2021-11-16 16:22:46 +0100

    ..

--- beansAppProject/src/main/java/africa/semicolon/beansAppProject/exception/EmailNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package africa.semicolon.beansAppProject.exception;
-
-public class EmailNotFoundException extends RuntimeException{
-    public EmailNotFoundException(String message) {
-        super(message);
-    }
-}
