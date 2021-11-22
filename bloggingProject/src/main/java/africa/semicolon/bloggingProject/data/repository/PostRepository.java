07cb7c69d263671ce06b2750c8b1f02964971e17
kindyluv <onyeukwuamara@gmail.com>
2021-11-22 10:50:41 +0100

    ...

--- bloggingProject/src/main/java/africa/semicolon/bloggingProject/data/repository/PostRepository.java
+++ bloggingProject/src/main/java/africa/semicolon/bloggingProject/data/repository/PostRepository.java
@@ -3,12 +3,15 @@ package africa.semicolon.bloggingProject.data.repository;
 import africa.semicolon.bloggingProject.data.model.Author;
 import africa.semicolon.bloggingProject.data.model.Comment;
 import africa.semicolon.bloggingProject.data.model.Post;
+import africa.semicolon.bloggingProject.exception.EmptyPostIdException;
+import africa.semicolon.bloggingProject.exception.PostTrackingIdNotFoundException;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PostRepository {
-    Post findPostByUniqueId(Integer postId);
-    Post makePost(Integer postId, Post post);
+    Optional<Post> findPostByUniqueId(Integer postId) throws PostTrackingIdNotFoundException;
+    Post makePost(Integer postId, Post post) throws PostTrackingIdNotFoundException;
     Post savePost(Post post);
     Post updatePost(Integer postId, Post newPost);
     Author saveAuthor(Author author);
@@ -18,5 +21,4 @@ public interface PostRepository {
     List<Post> findAllPost();
     Post findPost(Integer postId);
 
-    int getCount();
 }
