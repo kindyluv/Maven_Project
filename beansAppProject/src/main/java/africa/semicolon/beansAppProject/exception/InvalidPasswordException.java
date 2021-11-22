42b05cb389ab6cf646f74f2b0a8fab401aece3c8
kindyluv <onyeukwuamara@gmail.com>
2021-11-22 10:50:32 +0100

    ...

---
+++ beansAppProject/src/main/java/africa/semicolon/beansAppProject/exception/InvalidPasswordException.java
@@ -0,0 +1,8 @@
+package africa.semicolon.beansAppProject.exception;
+
+public class InvalidPasswordException extends RuntimeException {
+
+    public InvalidPasswordException(String message) {
+        super(message);
+    }
+}
