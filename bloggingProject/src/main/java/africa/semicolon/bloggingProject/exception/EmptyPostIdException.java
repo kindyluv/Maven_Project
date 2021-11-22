ecd364f0aec3557f5a05e2dade973993dd3d62c7
kindyluv <onyeukwuamara@gmail.com>
2021-11-22 10:50:15 +0100

    ...

---
+++ bloggingProject/src/main/java/africa/semicolon/bloggingProject/exception/EmptyPostIdException.java
@@ -0,0 +1,8 @@
+package africa.semicolon.bloggingProject.exception;
+
+public class EmptyPostIdException extends Exception {
+
+    public EmptyPostIdException(String message) {
+        super(message);
+    }
+}
