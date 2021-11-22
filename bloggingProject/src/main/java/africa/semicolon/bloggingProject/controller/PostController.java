7a653b351eccfa22fae9e3445e0329a7dae71c78
kindyluv <onyeukwuamara@gmail.com>
2021-11-22 10:50:44 +0100

    ...

--- bloggingProject/src/main/java/africa/semicolon/bloggingProject/controller/PostController.java
+++ bloggingProject/src/main/java/africa/semicolon/bloggingProject/controller/PostController.java
@@ -3,29 +3,46 @@ package africa.semicolon.bloggingProject.controller;
 import africa.semicolon.bloggingProject.data.model.Post;
 import africa.semicolon.bloggingProject.dtos.request.AddPostRequest;
 import africa.semicolon.bloggingProject.dtos.response.AddPostResponse;
+import africa.semicolon.bloggingProject.exception.DuplicatePostException;
+import africa.semicolon.bloggingProject.exception.DuplicatePostTrackingIdException;
+import africa.semicolon.bloggingProject.exception.PostTrackingIdNotFoundException;
 import africa.semicolon.bloggingProject.service.PostService;
 import africa.semicolon.bloggingProject.service.PostServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 public class PostController {
-    private PostService postService = new PostServiceImpl();
+    private final PostService postService = new PostServiceImpl();
 
     @PostMapping("/api/createPost")
-    public AddPostResponse createPost(@RequestBody AddPostRequest addPostRequest){
-        return postService.createPost(addPostRequest);
+    public ResponseEntity<?> createPost(@RequestBody AddPostRequest addPostRequest){
+       try{
+           AddPostResponse response = postService.createPost(addPostRequest);
+           return new ResponseEntity<>(response, HttpStatus.CREATED);
+       }catch(DuplicatePostException | DuplicatePostTrackingIdException e){
+           return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
+       }
     }
 
     @PatchMapping("/api/updatePost")
-    public Post updatePost(Integer postId, AddPostRequest addPostRequest){
-        return postService.updatePost(postId, addPostRequest);
+    public ResponseEntity<?> updatePost(Integer postId, AddPostRequest addPostRequest) throws PostTrackingIdNotFoundException {
+       Post post = postService.updatePost(postId, addPostRequest);
+        return new ResponseEntity<>(post, HttpStatus.OK);
     }
 
-    public void deletePost(Integer postId){
+    public void deletePost(Integer postId) throws PostTrackingIdNotFoundException {
         postService.deletePost(postId);
     }
 
     @GetMapping("/api/findPostById")
-    public Post findPostById(@PathVariable Integer id){
-        return postService.findPostByUniqueId(id);
+    public ResponseEntity<?> findPostById(@PathVariable Integer id){
+        try{
+            Post post = postService.findPostByUniqueId(id);
+            return new ResponseEntity<>(post, HttpStatus.ACCEPTED);
+        }catch(PostTrackingIdNotFoundException e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+
     }
 }
