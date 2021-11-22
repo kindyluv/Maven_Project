a73fccabf95d637af9fe37cec66708c4c7c9ee81
kindyluv <onyeukwuamara@gmail.com>
2021-11-22 10:48:15 +0100

    ...

--- bloggingProject/src/main/java/africa/semicolon/bloggingProject/controller/AuthorController.java
+++ bloggingProject/src/main/java/africa/semicolon/bloggingProject/controller/AuthorController.java
@@ -3,8 +3,12 @@ package africa.semicolon.bloggingProject.controller;
 import africa.semicolon.bloggingProject.data.model.Author;
 import africa.semicolon.bloggingProject.dtos.request.AddAuthorRequest;
 import africa.semicolon.bloggingProject.dtos.response.AddAuthorResponse;
+import africa.semicolon.bloggingProject.exception.DuplicateEmailException;
+import africa.semicolon.bloggingProject.exception.TrackingIdNotFoundException;
 import africa.semicolon.bloggingProject.service.AuthorService;
 import africa.semicolon.bloggingProject.service.AuthorServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,11 +16,21 @@ public class AuthorController {
     private final AuthorService authorService = new AuthorServiceImpl();
 
     @PostMapping("/api/author")
-    public AddAuthorResponse createAuthor(@RequestBody AddAuthorRequest request){
-       return authorService.createAuthor(request);
+    public ResponseEntity<?> createAuthor(@RequestBody AddAuthorRequest request){
+        try{
+            AddAuthorResponse response = authorService.createAuthor(request);
+            return new ResponseEntity<>(response, HttpStatus.CREATED);
+        }catch(DuplicateEmailException e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
     @GetMapping("/api/author/{id}")
-    public Author findAuthorById(@PathVariable("id")Integer authorId){
-        return authorService.findAuthorById(authorId);
+    public ResponseEntity<?> findAuthorById(@PathVariable("id")Integer authorId){
+        try{
+            Author author = authorService.findAuthorById(authorId);
+            return new ResponseEntity<>(author, HttpStatus.ACCEPTED);
+        }catch(TrackingIdNotFoundException e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
