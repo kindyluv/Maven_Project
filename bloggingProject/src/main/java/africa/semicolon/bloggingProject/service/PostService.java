b0972defc7a69166f7e99c9852afea9dc95c8dc1
kindyluv <onyeukwuamara@gmail.com>
2021-11-22 10:51:03 +0100

    ...

--- bloggingProject/src/main/java/africa/semicolon/bloggingProject/service/PostService.java
+++ bloggingProject/src/main/java/africa/semicolon/bloggingProject/service/PostService.java
@@ -3,12 +3,13 @@ package africa.semicolon.bloggingProject.service;
 import africa.semicolon.bloggingProject.data.model.Post;
 import africa.semicolon.bloggingProject.dtos.request.AddPostRequest;
 import africa.semicolon.bloggingProject.dtos.response.AddPostResponse;
+import africa.semicolon.bloggingProject.exception.PostTrackingIdNotFoundException;
 
 public interface PostService {
 
-    Post findPostByUniqueId(Integer Id);
+    Post findPostByUniqueId(Integer Id) throws PostTrackingIdNotFoundException;
     AddPostResponse createPost(AddPostRequest addPostRequest);
-    Post updatePost(Integer postId, AddPostRequest addPostRequest);
-    void deletePost(Integer postId);
+    Post updatePost(Integer postId, AddPostRequest addPostRequest) throws PostTrackingIdNotFoundException;
+    void deletePost(Integer postId) throws PostTrackingIdNotFoundException;
             
 }
