885ff3a11195ce0b512993a074ecad30f03f315f
kindyluv <onyeukwuamara@gmail.com>
2021-11-22 10:50:20 +0100

    ...

---
+++ beansAppProject/src/main/java/africa/semicolon/beansAppProject/exception/IncompleteFilledException.java
@@ -0,0 +1,8 @@
+package africa.semicolon.beansAppProject.exception;
+
+public class IncompleteFilledException extends RuntimeException{
+
+    public IncompleteFilledException(String message){
+        super(message);
+    }
+}
