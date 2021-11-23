c7cea708b5cf108cb71374af34bc2301d0bcc1e1
kindyluv <onyeukwuamara@gmail.com>
2021-11-23 12:41:46 +0100

    ...

---
+++ jijiMoji/src/main/java/africa/semicolon/jijiMoji/data/model/Condition.java
@@ -0,0 +1,5 @@
+package africa.semicolon.jijiMoji.data.model;
+
+public enum Condition {
+    USED, NEW, REFURBISHED;
+}
---
+++ jijiMoji/src/main/java/africa/semicolon/jijiMoji/data/model/Lister.java
@@ -0,0 +1,10 @@
+package africa.semicolon.jijiMoji.data.model;
+
+import org.springframework.data.mongodb.core.mapping.DBRef;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lister {
+    private String location;
+}
